c9a8b6525490ade8ee3aca066918bdc3c5795c31
Otmane Aatik <aaotmane@gmail.com>
2022-01-17 13:49:56 -0500

    Node callbacks and errors

--- rectangle.js
+++ rectangle.js
@@ -1,2 +1,20 @@
-exports.perimeter = (x, y) => 2 * (x + y) + 1;
-exports.area = (x, y) => x * y;
+module.exports = (x, y, callback) => {
+    if (x <= 0 || y <= 0) {
+        callback(new Error(`values must be greater than Zero : ${x}, ${y}`))
+    } else {
+        setTimeout(() => 
+            callback(null, {
+                perimeter: () => 2 * (x + y),
+                area: () => x * y
+            }),
+            2000
+        );
+    }
+}
+
+
+
+
+
+
+
